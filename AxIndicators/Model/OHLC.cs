24416eb875d257faacc3ce638cfb51d2c9edd0ca
Elham Souri <ElhamSouri@outlook.com>
2022-06-12 16:18:55 +0430

    Create OHLC and Indicator classes

---
+++ AxIndicators/Model/OHLC.cs
@@ -0,0 +1,24 @@
+﻿namespace AxIndicators.Model
+{
+    public class OHLC
+    {
+        public long Id { get; set; }
+        public int Index { get; set; }
+        public decimal Open { get; set; }
+        public decimal Close { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Change { get; set; }
+        public decimal Gain { get; set; }
+        public decimal Loss { get; set; }
+        public decimal AvgGain { get; set; }
+        public decimal AvgLoss { get; set; }
+        public decimal Rs { get; set; }
+        public decimal Rsi { get; set; }
+        public DateTime OpenTime { get; set; }
+        public DateTime CloseTime { get; set; }
+        public double Volume { get; set; }
+        public long OpenTimeTicks { get; set; }
+        public decimal AdjClose { get; set; }
+    }
+}
