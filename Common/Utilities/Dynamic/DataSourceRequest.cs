430ea305f42c702327847ec1909d17551fcf0150
Amir Norouzpour <Amirnorozpour@gmail.com>
2022-07-01 11:31:42 +0430

    add ToDatSourceResult

---
+++ Common/Utilities/Dynamic/DataSourceRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Utilities.Dynamic
+{
+  public  class DataSourceRequest
+    {
+        public int Take { get; set; }
+        public int Skip { get; set; }
+        public IEnumerable<Sort> Sort { get; set; }
+        public Filter Filter { get; set; }
+    }
+}
